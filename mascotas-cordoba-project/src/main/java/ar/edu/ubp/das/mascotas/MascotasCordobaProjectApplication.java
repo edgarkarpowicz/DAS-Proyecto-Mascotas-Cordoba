cdb57439062e081de6b627551a7d3eff73edcc53
Edgar <edgarkarpowicz@live.com>
2026-09-14 17:58:59 -0300

    Project created alongside it's structure + Added Basic Configuration

---
+++ mascotas-cordoba-project/src/main/java/ar/edu/ubp/das/mascotas/MascotasCordobaProjectApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.ubp.das.mascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotasCordobaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MascotasCordobaProjectApplication.class, args);
+    }
+
+}
---
+++ mascotas-cordoba-project/src/test/java/ar/edu/ubp/das/mascotas/MascotasCordobaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.ubp.das.mascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotasCordobaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
